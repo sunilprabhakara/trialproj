26221a31cb1116704ffc11e5b7e1edbd6105233a
Sunil Prabhakara <sunilcloudacc1@trial00646.ap.talend.com>
2019-03-26 19:15:11 +0530

    User 'sunilcloudacc1@trial00646.ap.talend.com' at 19:15:10 IST 2019-03-26 : save process : MySecondJob   Save process item 'MySecondJob' at path '' in project 'TrialProj'   update poms by save   Save project 'TrialProj'

---
+++ TRIALPROJ/poms/jobs/process/mysecondjob_0.1/src/main/java/internal/TalendJavaSourceViewer3.java
@@ -0,0 +1,36 @@
+package internal;
+
+import routines.DQTechnical;
+import routines.DataMasking;
+import routines.DataOperation;
+import routines.DataQualityDependencies;
+import routines.DataQuality;
+import routines.DqStringHandling;
+import routines.Mathematical;
+import routines.Numeric;
+import routines.Relational;
+import routines.SQLike;
+import routines.StringHandling;
+import routines.TalendDataGenerator;
+import routines.TalendDate;
+import routines.TalendStringUtil;
+import routines.TalendString;
+import routines.system.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.math.BigDecimal;
+
+@SuppressWarnings("unused")
+
+public class TalendJavaSourceViewer3 {
+	private static java.util.Properties context = new java.util.Properties();
+	private static final java.util.Map<String, Object> globalMap = new java.util.HashMap<String, Object>();
+	public void myFunction(){
+	  if( 
+System.out.println("Hope it works ...");){
+	}
+	
+}
+}
