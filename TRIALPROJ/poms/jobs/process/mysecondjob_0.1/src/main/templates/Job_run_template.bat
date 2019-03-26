e5da32c32d92f0cdba7f0bde65145705a5718792
Sunil Prabhakara <sunilcloudacc1@trial00646.ap.talend.com>
2019-03-26 19:15:35 +0530

    User 'sunilcloudacc1@trial00646.ap.talend.com' at 19:15:18 IST 2019-03-26 : User action : Publish to Cloud

---
+++ TRIALPROJ/poms/jobs/process/mysecondjob_0.1/src/main/templates/Job_run_template.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Dtalend.component.manager.m2.repository=%cd%/../lib -Xms256M -Xmx1024M -cp .;../lib/routines.jar;../lib/antlr-runtime-3.5.2.jar;../lib/dom4j-1.6.1.jar;../lib/log4j-1.2.17.jar;../lib/org.talend.dataquality.parser.jar;../lib/talend_file_enhanced_20070724.jar;mysecondjob_0_1.jar; trialproj.mysecondjob_0_1.MySecondJob  --context=Default %*
